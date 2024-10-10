f1e9babf516d6af7318377f83c59ee8320ba6de1
rezmes <rezmes@gmail.com>
2024-10-10 13:56:04 +0330

    finished

--- src/webparts/jqExtLib/JqExtLibWebPart.ts
+++ src/webparts/jqExtLib/JqExtLibWebPart.ts
@@ -9,28 +9,99 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import styles from './JqExtLibWebPart.module.scss';
 import * as strings from 'JqExtLibWebPartStrings';
 
+import { SPComponentLoader} from '@microsoft/sp-loader';
+import * as $ from 'jquery';
+import "jqueryui"
+
+
+
+
+
 export interface IJqExtLibWebPartProps {
   description: string;
 }
 
 export default class JqExtLibWebPart extends BaseClientSideWebPart<IJqExtLibWebPartProps> {
 
+constructor() {
+  super();
+  SPComponentLoader.loadCss('https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css');
+
+  // SPComponentLoader.loadScript('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', { globalExportsName: 'jQuery' });
+  // SPComponentLoader.loadScript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', { globalExportsName: 'jQuery' });
+  // SPComponentLoader.loadScript('https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js', { globalExportsName: 'jQuery' });
+}
+
+
+
+
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.jqExtLib }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>`;
+<div class = {${styles.jqExtLib}}>
+
+<div  class = "accordion">
+
+    <h3>JQExtLib 1</h3>
+
+
+ <div>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+  </ul>
+ </div>
+
+    <h3>JQExtLib 2</h3>
+
+
+ <div>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+  </ul>
+ </div>
+
+    <h3>JQExtLib 3</h3>
+
+
+ <div>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+  </ul>
+ </div>
+
+
+
+
+
+
+
+
+ </div>
+
+
+
+</div>`;
+
+
+
+      const accordionOptions = {
+animate: true,
+collapsible: true,
+icons: {
+  header: "ui-icon-triangle-1-s",
+  activeHeader: "ui-icon-triangle-1-s"
+}
+      };
+
+      ($('.accordion', this.domElement) as any).accordion(accordionOptions);
   }
 
   protected get dataVersion(): Version {
